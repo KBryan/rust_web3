f5901729b15985f16314c7271cd0151a4a091caf
Kwame Bryan <kwame.bryan@gmail.com>
2021-08-24 18:07:00 -0400

    Rust using web3 updates

--- src/lib.rs
+++ src/lib.rs
@@ -10,16 +10,16 @@ use web3::{
 /// Below sends a transaction to a local node that stores private keys (eg Ganache)
 /// For generating and signing a transaction offline, before transmitting it to a public node (eg Infura) see transaction_public
 async fn call_web3() -> web3::Result<()> {
-    // run local gananche
+
     let transport = web3::transports::Http::new("HTTP://127.0.0.1:8545").unwrap();
     let web3 = web3::Web3::new(transport);
 
 
     // Insert the 20-byte "from" address in hex format (prefix with 0x)
-    let from = Address::from_str("add_account_1_gananche").unwrap();
+    let from = Address::from_str("0xb884FF4b46C7d0cCb68d280EF39B3cBE8Fd47590").unwrap();
 
     // Insert the 20-byte "to" address in hex format (prefix with 0x)
-    let to = Address::from_str("add_account_2_gananche").unwrap();
+    let to = Address::from_str("0x7b953144Da42fe99d15411ae9e945C877b1B839c").unwrap();
 
     // Build the tx object
     let tx_object = TransactionRequest {
@@ -39,5 +39,8 @@ async fn call_web3() -> web3::Result<()> {
 #[tokio::main]
 async fn main() {
 
-     task::block_on(call_web3());
+    #[no_mangle]
+    pub extern fn mob_web3() -> () {
+        task::block_on(call_web3());
+    }
 }
